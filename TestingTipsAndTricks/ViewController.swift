ca6fe41ff717149cba13f97960064b155c611389
hongten <m10117013@gmail.com>
2018-08-24 02:44:58 +0800

    Initial Commit

---
+++ TestingTipsAndTricks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingTipsAndTricks
+//
+//  Created by wei on 2018/8/24.
+//  Copyright © 2018年 wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
