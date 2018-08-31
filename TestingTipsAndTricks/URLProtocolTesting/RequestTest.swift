91d745c8cc63c1b101e55d7d1c1eaab7c6b14394
hongten <m10117013@gmail.com>
2018-08-31 16:42:55 +0800

    Initial commit

---
+++ TestingTipsAndTricks/URLProtocolTesting/RequestTest.swift
@@ -0,0 +1,91 @@
+//
+//  RequestTest.swift
+//  TestingTipsAndTricks
+//
+//  Created by wei on 2018/8/24.
+//  Copyright © 2018年 wei. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+enum RequestError:Error {
+    case invalidCoordinate
+    case tooYoungProblem
+    case notAquariusProblem
+    case falseHeartProblem
+}
+
+struct PointOfInterest : Codable {
+    var name : String
+}
+
+extension PointOfInterest: Equatable {}
+
+func ==(lhs: PointOfInterest, rhs: PointOfInterest) -> Bool {
+    let areEqual = lhs.name == rhs.name
+    
+    return areEqual
+}
+
+
+protocol APIRequest {
+    associatedtype RequestDataType
+    associatedtype ResponseDataType
+    func makeRequest(from data: RequestDataType) throws -> URLRequest
+    func parseResponse(data: Data) throws -> ResponseDataType
+}
+
+
+//protocol APIRequest {
+//    associatedtype RequestDataType
+//    associatedtype ResponseDataType
+//    func makeRequest(from data: TCStokItem) throws -> URLRequest
+//    func parseResponse(data: Data) throws -> TCResponse
+//}
+
+struct PointsOfInterestRequest : APIRequest {
+    func makeRequest(from coordinate: CLLocationCoordinate2D) throws -> URLRequest {
+        guard CLLocationCoordinate2DIsValid(coordinate) else {
+            throw RequestError.invalidCoordinate
+        }
+        var components = URLComponents(string: "https://example.com/locations")!
+        components.queryItems = [
+            URLQueryItem(name: "lat", value: "\(coordinate.latitude)"),
+            URLQueryItem(name: "long", value: "\(coordinate.longitude)")
+        ]
+        
+        return URLRequest(url: components.url!)
+    }
+    
+    
+    func parseResponse(data: Data) throws -> [PointOfInterest] {
+        return try JSONDecoder().decode([PointOfInterest].self, from: data)
+    }
+}
+
+class APIRequestLoader<T: APIRequest> {
+
+    let apiRequest: T
+    let urlSession: URLSession
+    init(apiRequest: T, urlSession: URLSession = .shared) {
+        self.apiRequest = apiRequest
+        self.urlSession = urlSession
+    }
+    
+    func loadAPIRequest(requestData: T.RequestDataType,
+                        completionHandler: @escaping (T.ResponseDataType?, Error?) -> Void) {
+        do {
+            let urlRequest = try apiRequest.makeRequest(from: requestData)
+            urlSession.dataTask(with: urlRequest) { data, response, error in
+                guard let data = data else { return completionHandler(nil, error) }
+                do {
+                    let parsedResponse = try self.apiRequest.parseResponse(data: data)
+                    completionHandler(parsedResponse, nil)
+                } catch {
+                    completionHandler(nil, error)
+                }
+            }.resume()
+        } catch { return completionHandler(nil, error) }
+    }
+}
--- TestingTipsAndTricksTests/TestingTipsAndTricksTests.swift
+++ TestingTipsAndTricksTests/TestingTipsAndTricksTests.swift
@@ -7,12 +7,23 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import TestingTipsAndTricks
 
 class TestingTipsAndTricksTests: XCTestCase {
     
+    var loader: APIRequestLoader<PointsOfInterestRequest>!
+    
+    ///初始化代碼
     override func setUp() {
         super.setUp()
+        
+        let request = PointsOfInterestRequest()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        let urlSession = URLSession(configuration: configuration)
+        
+        loader = APIRequestLoader(apiRequest: request, urlSession: urlSession)
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -21,9 +32,26 @@ class TestingTipsAndTricksTests: XCTestCase {
         super.tearDown()
     }
     
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let inputCoordinate = CLLocationCoordinate2D(latitude: 37.3293, longitude: -121.8893)
+        let mockJSONData = "[{\"name\":\"MyPointOfInterest\"}]".data(using: .utf8)!
+        MockURLProtocol.requestHandler = { request in
+            XCTAssertEqual(request.url?.query?.contains("lat=37.3293"), true)
+            return (HTTPURLResponse(), mockJSONData)
+        }
+        
+        
+        let expectation = XCTestExpectation(description: "response")
+        loader.loadAPIRequest(requestData: inputCoordinate) { pointsOfInterest, error in
+            XCTAssertEqual(pointsOfInterest!, [PointOfInterest(name: "MyPointOfInterest")])
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1000)
+        
+        
     }
     
     func testPerformanceExample() {
@@ -34,3 +62,5 @@ class TestingTipsAndTricksTests: XCTestCase {
     }
     
 }
+
+
